9a25cb5511c75f958914a4f4b70abd88a7402578
Jerico Luis Lua <jericoluislua@yahoo.com.ph>
2020-09-01 14:45:06 -0700

    Fixed some styles

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,6 @@ class Book < ApplicationRecord
   #https://github.com/rails/rails/issues/24518
   belongs_to :subject, optional: true
   #validates_presence_of :title, message:" can't be left empty"
-  validates :title, presence: true, message: "can't be left empty"
+  validates :title, presence: true
   validates_numericality_of :price, message:"not added/Non numerical included"
 end
